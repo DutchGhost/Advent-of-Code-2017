272584d446ebad3a574fa15ef6c87f8e90d8f252
Dutch_Gh0st <kasper199914@gmail.com>
2017-12-01 17:55:20 +0100

    char to u32
    Change transformation of char to u32 only when we've found a valid thing

--- Rust/day1/src/main.rs
+++ Rust/day1/src/main.rs
@@ -1,27 +1,18 @@
-#![feature(conservative_impl_trait)]
-fn to_num<I: Iterator<Item=char> + Clone>(iter: I) -> impl Iterator<Item=u32> + Clone {
-    iter.map(to_digit)
-}
-
-fn to_digit(c: char) -> u32 {
-    c.to_digit(10).unwrap()
-}
-
 const PUZZLE: &'static str = include_str!("PUZZLE.txt");
-fn main() {
 
-    println!("day 1.1: {}", summenize(to_num(PUZZLE.chars()), 1));
-    println!("day 1.2: {}", summenize(to_num(PUZZLE.chars()), PUZZLE.len() / 2));
+fn main() {
+    println!("day 1.1: {}", summenize(PUZZLE.chars(), 1));
+    println!("day 1.2: {}", summenize(PUZZLE.chars(), PUZZLE.len() / 2));
 }
 
 ///take an Iterator, zip with an endless version of itself that skips for `skip`.
 fn summenize<'a, I>(iter: I, skip: usize) -> u32
 where
-    I: Iterator<Item =u32> + Clone,
+    I: Iterator<Item = char> + Clone,
 {
     iter.clone()
         .zip(iter.cycle().skip(skip))
         .filter(|&(first, second)| first == second)
-        .map(|(first, _)| first)
+        .map(|(first, _)| first.to_digit(10).unwrap())
         .sum()
 }
