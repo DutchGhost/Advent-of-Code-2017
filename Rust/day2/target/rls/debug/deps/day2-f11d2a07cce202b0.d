eec28703ac4dc4faae1808126d63b0d245433721
DutchGhost <kasper199914@gmail.com>
2017-12-02 09:30:06 +0100

    added gitnore

--- Rust/day2/target/rls/debug/deps/day2-f11d2a07cce202b0.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Kasper\Documents\Advent-of-Code\Rust\day2\target\rls\debug\deps\day2-f11d2a07cce202b0.rmeta: C:\Users\Kasper\Documents\Advent-of-Code\Rust\day2\src\main.rs
-
-c:\Users\Kasper\Documents\Advent-of-Code\Rust\day2\target\rls\debug\deps\day2-f11d2a07cce202b0.d: C:\Users\Kasper\Documents\Advent-of-Code\Rust\day2\src\main.rs
-
-C:\Users\Kasper\Documents\Advent-of-Code\Rust\day2\src\main.rs:
