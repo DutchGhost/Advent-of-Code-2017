60dbbd945b3c9f0f310a0a4f3ea22e53a764df88
DutchGhost <kasper199914@gmail.com>
2017-12-02 10:40:08 +0100

    made it more efficient. Only needed to convert to nums once

--- Rust/day2/src/main.rs
+++ Rust/day2/src/main.rs
@@ -1,14 +1,14 @@
 const PUZZLE: &'static str = include_str!("Input.txt");
 
 fn to_num(s: &str) -> Vec<u32> {
-    s.split('\t')
+    s.split_whitespace()
         .map(|n| n.parse::<u32>().unwrap())
         .collect::<Vec<_>>()
 }
 
-fn evenly(vec: Vec<u32>) -> u32 {
+fn evenly(vec: &[u32]) -> u32 {
     for first in vec.iter() {
-        for second in vec.iter().filter(|item| item != &first) {
+        for second in vec.iter().rev().filter(|item| item != &first) {
             if first % second == 0 {
                 return first / second;
             }
@@ -16,22 +16,29 @@ fn evenly(vec: Vec<u32>) -> u32 {
     }
     unreachable!()
 }
+
+fn difference(nums: &[u32]) -> u32 {
+    nums.iter().max().unwrap() - nums.iter().min().unwrap()
+}
+
+fn solve() -> (u32, u32) {
+    let parsed = PUZZLE.lines().map(|line| to_num(line)).collect::<Vec<_>>();
+    
+    let part1 = parsed
+                .iter()
+                .map(|nums| difference(nums))
+                .sum::<u32>();
+    
+    let part2 = parsed
+                .iter()
+                .map(|nums| evenly(nums))
+                .sum::<u32>();
+
+    (part1, part2)
+}
 fn main() {
-    println!(
-        "day 1.1: {}",
-        PUZZLE
-            .lines()
-            .map(|line| to_num(line))
-            .map(|nums| {
-                nums.iter().max().unwrap() - nums.iter().min().unwrap()
-            })
-            .sum::<u32>()
-    );
-    println!(
-        "day 1.2: {}",
-        PUZZLE
-            .lines()
-            .map(|line| evenly(to_num(line)))
-            .sum::<u32>()
-    );
+    let (part1, part2) = solve();
+
+    println!("day 2.1: {}", part1);
+    println!("day 2.2: {}", part2);
 }
