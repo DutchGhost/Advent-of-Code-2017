6acf7800a835bcd32e97ec5ec501bbc8f7b96364
DutchGhost <kasper199914@gmail.com>
2017-12-03 21:23:16 +0100

    removal of target folder

--- Rust/day3/target/release/day3.d
+++ Rust/day3/target/release/day3.d
@@ -1,1 +1,1 @@
-D:\Kasper\Documenten\AoC2017\Rust\day3\target\release\day3.exe: D:\Kasper\Documenten\AoC2017\Rust\day3\src\spiral.rs D:\Kasper\Documenten\AoC2017\Rust\day3\src\genitter.rs D:\Kasper\Documenten\AoC2017\Rust\day3\src\main.rs
+D:\Kasper\Documenten\AoC2017\Rust\day3\target\release\day3.exe: D:\Kasper\Documenten\AoC2017\Rust\day3\src\spiral.rs D:\Kasper\Documenten\AoC2017\Rust\day3\src\main.rs D:\Kasper\Documenten\AoC2017\Rust\day3\src\genitter.rs
