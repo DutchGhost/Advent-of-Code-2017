391eb444c4b6fd2905da3feae1dcbdf1a2fbf0bb
DutchGhost <kasper199914@gmail.com>
2017-12-03 21:24:09 +0100

    removal of target

--- Rust/day3/target/release/day3.d
+++
@@ -1,1 +0,0 @@
-D:\Kasper\Documenten\AoC2017\Rust\day3\target\release\day3.exe: D:\Kasper\Documenten\AoC2017\Rust\day3\src\spiral.rs D:\Kasper\Documenten\AoC2017\Rust\day3\src\main.rs D:\Kasper\Documenten\AoC2017\Rust\day3\src\genitter.rs
--- Rust/day3/target/rls/debug/libday3.d
+++
@@ -1,1 +0,0 @@
-d:\Kasper\Documenten\AoC2017\Rust\day3\target\rls\debug\libday3.rmeta: D:\Kasper\Documenten\AoC2017\Rust\day3\src\main.rs
