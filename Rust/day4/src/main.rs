c2072ff0a605e5e789b8fd56be68fc1d2b18fa94
DutchGhost <kasper199914@gmail.com>
2017-12-04 12:59:11 +0100

    day 4.2

--- Rust/day4/src/main.rs
+++ Rust/day4/src/main.rs
@@ -18,4 +18,22 @@ fn main() {
         }
     }
     println!("{}", n);
+
+    let mut m = 0;
+    for line in PUZZLE.lines() {
+        //let mut tmp = Vec::new();
+        let mut map = HashMap::new();
+        for word in line.split_whitespace() {
+            let mut arr = word.chars().collect::<Vec<_>>();
+            arr.sort();
+            *map.entry(arr).or_insert(1) += 1;
+        }
+        for (key, value) in map.iter() {
+            println!("{:?}, {}", key, value);
+        }
+        if map.values().all(|value| value == &2) {
+            m += 1;
+        }
+    }
+    println!("{}", m)
 }
