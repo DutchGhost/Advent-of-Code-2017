6f883cb054912f98e9aefeadbc62050debbf4876
DutchGhost <kasper199914@gmail.com>
2017-12-04 21:18:01 +0100

    UGH

--- Rust/day4/src/main.rs
+++ Rust/day4/src/main.rs
@@ -1,27 +1,25 @@
-extern crate permutohedron;
 const PUZZLE: &'static str = include_str!("Input.txt");
 
-use permutohedron::heap_recursive;
-
-use std::collections::HashMap;
 use std::collections::HashSet;
 
+/// Iterate over the lines,
+/// in each lines iterate over the words.
+/// Put each word into the HashSet,
+/// if the number of elements in the HashSet equals the lenght of the line,
+/// its a valid line.
 fn part1(input: &str) -> i32 {
     let mut valids = 0;
-    let mut map = HashMap::new();
+    let mut map = HashSet::new();
     
-    /// Iterate over the lines
     for line in input.lines() {
         let mut count = 0;
         
-        // Iterate over each word, and put it in a hashmap
         for word in line.split_whitespace() {
-            *map.entry(word).or_insert(0) += 1;
+            map.insert(word);
             count += 1;
         }
 
-        // If the count of the keys equals to the count of the words in the line, it's a valid line.
-        if map.keys().count() == count {
+        if map.iter().count() == count {
             valids += 1;
         }
         map.drain();
@@ -29,24 +27,27 @@ fn part1(input: &str) -> i32 {
     valids
 }
 
+/// Iterate over the lines,
+/// in each line iterate over the words,
+/// and store the characters of the words in a vector.
+/// Then sort the vector, and put in the HashSet.
+/// If the number of elements in the HashSet equals the lenght of the line,
+/// it's a vlid line
 fn part2(input: &str) -> i64 {
     let mut valids = 0;
-    let mut map = HashMap::new();
+    let mut map = HashSet::new();
     
-    // Iterate over the lines
     for line in input.lines() {
         let mut count = 0;
         
-        // Iterate over each word, sort the word, and put in in a hashmap
         for word in line.split_whitespace() {
             let mut chars = word.chars().collect::<Vec<_>>();
             chars.sort();
-            *map.entry(chars).or_insert(1) += 1;
+            map.insert(chars);
             count += 1;
         }
         
-        // If the count of the keys eqals the count of the (sorted) words in the line, it's a valid line.
-        if map.keys().count() == count {
+        if map.iter().count() == count {
             valids += 1;
         }
         map.drain();
@@ -91,37 +92,4 @@ fn main() {
     let (part1, part2) = combined(PUZZLE);
     println!("{}", part1);
     println!("{}", part2);
-
-    let n = PUZZLE.lines().filter(|line| anagram(line)).count();
-    println!("{}", n);
-}
-
-fn permutations(mut vec: &mut Vec<char>, line: &str) -> Vec<String> {
-    let mut v = Vec::new();
-    heap_recursive(&mut vec, |permutation| {
-        v.push(permutation.iter().collect::<String>());
-    });
-    v
-}
-
-//we have line, we get the words out it. We also store the words in a vector.
-//we get the permutations of each word,
-//if the permutations of a word are more than 1 times in the vector, its NOT a valid line.
-fn anagram(line: &str) -> bool {
-    let v = line.split_whitespace().map(|word| word.to_string()).collect::<Vec<_>>();
-    for word in line.split_whitespace() {
-        let mut tmp = word.chars().collect::<Vec<_>>();
-        let permutations = permutations(&mut tmp, line);
-            
-        //get the occurence of a permutation in the vector.
-        for item in line.split_whitespace() {
-            let i = permutations.iter().filter(|perm| perm == &item).count() as i64;
-            if i > 0 {
-                println!("{} is {} times in the vector, but mutated\n{:?}", item, i, v);
-                return false
-            }
-        }
-        //if occurence == 2 {println!("{:?}, {}, {:?}", v, word, permutations.iter().find(|elem| elem.iter().collect::<String>() == word));return false;}
-    }
-    true
 }
