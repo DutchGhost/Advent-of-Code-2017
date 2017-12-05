fc1c4c51ba4e62f1bb889e34bf31e7c4c3a5dcc6
DutchGhost <kasper199914@gmail.com>
2017-12-05 07:24:24 +0100

    day 5 part 1

---
+++ Rust/day5/src/main.rs
@@ -0,0 +1,25 @@
+const PUZZLE: &'static str = include_str!("Input.txt");
+
+fn parse(input: &str) -> Vec<i64> {
+    input
+        .lines()
+        .map(|line| line.parse::<i64>().expect("this should not happend"))
+        .collect::<Vec<_>>()
+}
+
+fn main() {
+    let mut jumps = parse(PUZZLE);
+
+    let mut n = 0;
+    let mut prev: i64 = 0;
+    let mut pc: i64 = 0;
+    let mut lenght = jumps.len();
+
+    while pc < lenght as i64 {
+        pc += jumps[pc as usize];
+        jumps[prev as usize] += 1;
+        prev = pc;
+        n += 1;
+    }
+    println!("{}", n)
+}
