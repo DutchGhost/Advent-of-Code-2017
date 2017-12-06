e8673fffdcd86c1da9589465090bc60af8d295a7
DutchGhost <kasper199914@gmail.com>
2017-12-06 11:44:34 +0100

    changed it to a HashMap! and made use of the entry API

--- Rust/day6/src/main.rs
+++ Rust/day6/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::hash_map::*;
+
 const PUZZLE: &'static str = include_str!("Input.txt");
 
 fn parse(s: &str) -> Vec<i32> {
@@ -6,45 +8,49 @@ fn parse(s: &str) -> Vec<i32> {
         .collect::<Vec<_>>()
 }
 
-fn redistribute(memory: &mut [i32], mut idx: i64, mut value: i32) {
-    
-    memory[idx as usize] = 0;
-    
+fn redistribute(memory: &mut [i32], mut idx: usize, mut value: i32) {
+    memory[idx] = 0;
+
     idx += 1;
     let lenght = memory.len();
 
     while value > 0 {
 
-        idx = idx % lenght as i64;
-        memory[idx as usize] += 1;
+        idx = idx % lenght;
+        memory[idx] += 1;
         
         value -= 1;
         idx += 1;
     }
     
 }
-fn main() {
-    let mut memory = parse(PUZZLE);
-    let mut cache: Vec<(i64, Vec<i32>)> = Vec::new();
 
-    let mut part1 = 0;
+fn solve(input: &str) -> (i32, i32) {
+    let mut memory = parse(input);
+    let mut cache: HashMap<Vec<i32>, i32> = HashMap::new();
 
+    let mut n = 0;
     loop {
         let (idx, value) = memory
             .iter()
             .enumerate()
-            .map(move |(idx, bank)| (idx.clone() as i64, bank.clone()))
-            .max_by_key(|&(idx, bank)| (bank, -idx)).unwrap();
+            .map(|(idx, bank)| (idx, *bank))
+            .max_by_key(|&(idx, bank)| (bank, -(idx as i64))).unwrap();
 
         redistribute(&mut memory, idx, value);
-        part1 += 1;
+        n += 1;
 
-        if cache.iter().filter(|&&(_, ref cached)| *cached == memory).count() == 1 {
-            println!("part 2: {}", cache.iter().filter(|&&(_, ref cached)| *cached == memory).map(|&(n, _)| part1 - n).next().unwrap());
-            break;
-        }
-
-        cache.push((part1, memory.clone()));
+        match cache.entry(memory.clone()) {
+            Entry::Occupied(map) => {
+                return (n, n - map.get());
+            },
+            Entry::Vacant(memory) => memory.insert(n),
+        };
     }
+}
+
+fn main() {
+    let (part1, part2) = solve(PUZZLE);
     println!("part 1: {}", part1);
+    println!("part 2: {}", part2);
 }
