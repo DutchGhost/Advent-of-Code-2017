8730a74190cecd0e53444395f3e2b87e9839b2fb
DutchGhost <kasper199914@gmail.com>
2017-12-06 12:05:19 +0100

    changed the way for cycling, and redistrubiting

--- Rust/day6/src/main.rs
+++ Rust/day6/src/main.rs
@@ -8,21 +8,22 @@ fn parse(s: &str) -> Vec<i32> {
         .collect::<Vec<_>>()
 }
 
-fn redistribute(memory: &mut [i32], mut idx: usize, mut value: i32) {
+fn redistribute(memory: &mut [i32], idx: usize, value: i32) {
     memory[idx] = 0;
 
-    idx += 1;
-    let lenght = memory.len();
+    (0..memory.len())
+        .cycle()
+        .skip(idx + 1)
+        .take(value as usize)
+        .for_each(|idx| memory[idx] += 1);
+}
 
-    while value > 0 {
-
-        idx = idx % lenght;
-        memory[idx] += 1;
-        
-        value -= 1;
-        idx += 1;
-    }
-    
+fn cycle(memory: &[i32]) -> (usize, i32) {
+    memory
+        .iter()
+        .enumerate()
+        .map(|(idx, bank)| (idx, *bank))
+        .max_by_key(|&(idx, bank)| (bank, -(idx as i64))).unwrap()
 }
 
 fn solve(input: &str) -> (i32, i32) {
@@ -31,11 +32,7 @@ fn solve(input: &str) -> (i32, i32) {
 
     let mut n = 0;
     loop {
-        let (idx, value) = memory
-            .iter()
-            .enumerate()
-            .map(|(idx, bank)| (idx, *bank))
-            .max_by_key(|&(idx, bank)| (bank, -(idx as i64))).unwrap();
+        let (idx, value) = cycle(&memory);
 
         redistribute(&mut memory, idx, value);
         n += 1;
