6672f7fe43bd07bdbff60c4a3cb1cbe8933fcd22
DutchGhost <kasper199914@gmail.com>
2017-12-07 22:22:36 +0100

    nice part 1

--- Rust/day7/src/main.rs
+++ Rust/day7/src/main.rs
@@ -2,33 +2,6 @@ use std::collections::HashMap;
 
 const PUZZLE: &'static str = include_str!("Input.txt");
 
-fn values(input: &str) -> HashMap<String, i64> {
-    let mut values = HashMap::new();
-
-    for line in input.lines() {
-        let name_value = line.split_whitespace().take(2).collect::<Vec<_>>();
-
-        let value = name_value
-            .iter()
-            .nth(1)
-            .unwrap()
-            .chars()
-            .filter(|c| !(c == &'(' || c == &')'))
-            .collect::<String>()
-            .parse::<i64>()
-            .unwrap();
-
-        let name = name_value
-            .iter()
-            .nth(0)
-            .unwrap()
-            .to_string();
-
-        values.insert(name, value);
-    }
-    values
-}
-
 fn parse(input: &str) -> HashMap<String, String> {
     let mut map = HashMap::new();
     for line in input.lines() {
@@ -50,9 +23,6 @@ fn parse(input: &str) -> HashMap<String, String> {
 fn main() {
     //childs pointing to a parent. K is the child, V the parent.
     let mut set = parse(PUZZLE);
-    let values = values(PUZZLE);
-
-    let mut part2: HashMap<String, Vec<i64>> = HashMap::new();
 
     for (k, v) in set.iter() {
         if !set.contains_key(v) {
@@ -60,25 +30,24 @@ fn main() {
             break;
         }
     }
-
-    //child : parent
-    //get the parent, or insert an emptry vec
-    //then add the value of the child it.
-    //but maybe. the child IS also a parent.
-    for (k, v) in set.iter_mut() {
-        if is_child(k, &set) {
-            part2.entry(v.clone()).or_insert(Vec::new()).push(values.get(k).unwrap().clone())
-        }
-        //after we've had a child, we dont really need it anymore????
-    }
 }
 
-//if nothing points to check, check IS a child.
-fn is_child(check: &String, map: &HashMap<String, String>) -> bool {
-    for (child, parent) in map.iter() {
-        if parent == check {
-            return false
-        }
-    }
-    return true
-}
+/*
+           /     
+         ugml - ebii
+       /      \     
+      |         jptl
+      |        
+      |         pbga
+     /        /
+tknk --- padx - havc
+     \        \
+      |         qoyq
+      |             
+      |         ktlj
+       \      /     
+         fwft - cntj
+              \     
+                xhth
+
+*/      
