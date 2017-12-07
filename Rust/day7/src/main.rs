f16cc7a92a76e4ee7ea4b52ada570732f74f9296
DutchGhost <kasper199914@gmail.com>
2017-12-07 20:49:23 +0100

    ugh part 2

--- Rust/day7/src/main.rs
+++ Rust/day7/src/main.rs
@@ -2,6 +2,33 @@ use std::collections::HashMap;
 
 const PUZZLE: &'static str = include_str!("Input.txt");
 
+fn values(input: &str) -> HashMap<String, i64> {
+    let mut values = HashMap::new();
+
+    for line in input.lines() {
+        let name_value = line.split_whitespace().take(2).collect::<Vec<_>>();
+
+        let value = name_value
+            .iter()
+            .nth(1)
+            .unwrap()
+            .chars()
+            .filter(|c| !(c == &'(' || c == &')'))
+            .collect::<String>()
+            .parse::<i64>()
+            .unwrap();
+
+        let name = name_value
+            .iter()
+            .nth(0)
+            .unwrap()
+            .to_string();
+
+        values.insert(name, value);
+    }
+    values
+}
+
 fn parse(input: &str) -> HashMap<String, String> {
     let mut map = HashMap::new();
     for line in input.lines() {
@@ -21,11 +48,37 @@ fn parse(input: &str) -> HashMap<String, String> {
 }
 
 fn main() {
-    let set = parse(PUZZLE);
+    //childs pointing to a parent. K is the child, V the parent.
+    let mut set = parse(PUZZLE);
+    let values = values(PUZZLE);
+
+    let mut part2: HashMap<String, Vec<i64>> = HashMap::new();
+
     for (k, v) in set.iter() {
-        match set.get(v) {
-            Some(_) => continue,
-            None => {println!("{}", v); break}
+        if !set.contains_key(v) {
+            println!("part 1: {}", v);
+            break;
         }
     }
+
+    //child : parent
+    //get the parent, or insert an emptry vec
+    //then add the value of the child it.
+    //but maybe. the child IS also a parent.
+    for (k, v) in set.iter_mut() {
+        if is_child(k, &set) {
+            part2.entry(v.clone()).or_insert(Vec::new()).push(values.get(k).unwrap().clone())
+        }
+        //after we've had a child, we dont really need it anymore????
+    }
+}
+
+//if nothing points to check, check IS a child.
+fn is_child(check: &String, map: &HashMap<String, String>) -> bool {
+    for (child, parent) in map.iter() {
+        if parent == check {
+            return false
+        }
+    }
+    return true
 }
