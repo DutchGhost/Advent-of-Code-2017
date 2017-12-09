273f44e1ca0d99c554c0d30e1a96b0b0f466fa9f
DutchGhost <kasper199914@gmail.com>
2017-12-09 14:38:44 +0100

    now everything is in a good order

--- Rust/day9/src/main.rs
+++ Rust/day9/src/main.rs
@@ -3,9 +3,9 @@ const PUZZLE: &'static str = include_str!("Input.txt");
 //on a '!', we ignore the next character
 //on a '{', if we're not in some garbage, go up a group.
 //on a '}', if we're not in some garbage, update the score, and go down a group.
-//on a '>', we leave the garbage. Set garbage to false.
 //on a '<', if we're already IN some pile of garbage, increment the gccount.
 //  and set garbage always to true
+//on a '>', we leave the garbage. Set garbage to false.
 //otherewise, if we're currently in some garbage, increment the gccount.
 fn solve(input: &str) -> (i64, i64) {
     let mut group = 0;
@@ -21,8 +21,8 @@ fn solve(input: &str) -> (i64, i64) {
             '!'             => { cs.next(); },
             '{' if !garbage => group += 1,
             '}' if !garbage => { score += group; group -= 1; }
-            '>'             => garbage = false,
             '<'             => { if garbage { gccount += 1; } garbage = true; }
+            '>'             => garbage = false,
             _               => { gccount += 1; },
         };
     }
