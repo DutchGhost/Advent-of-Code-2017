3e859a0b7cf4da45e1448859ab630cdefc6210e5
DutchGhost <kasper199914@gmail.com>
2017-12-09 14:18:32 +0100

    tiny reformat

--- Rust/day9/src/main.rs
+++ Rust/day9/src/main.rs
@@ -1,11 +1,12 @@
 const PUZZLE: &'static str = include_str!("Input.txt");
+
 //on a '!', we ignore the next character
 //on a '<', if we're already IN some pile of garbage, increment the gccount.
 //  and set garbage always to true
 //on a '>', we leave the garbage. Set garbage to false.
 //on a '{', if we're not in some garbage, go up a level.
 //on a '}', if we're not in some garbage, update the score, and go down a level.
-//Otherewise, if we're currently in some garbage, increment the gccount.
+//otherewise, if we're currently in some garbage, increment the gccount.
 fn solve(input: &str) -> (i64, i64) {
     let mut level = 0;
     let mut score = 0;
