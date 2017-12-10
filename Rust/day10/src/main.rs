d09f7f367154d4806a24a8d30124cbda0b1dac68
DutchGhost <kasper199914@gmail.com>
2017-12-11 00:04:24 +0100

    usized all over the plece

--- Rust/day10/src/main.rs
+++ Rust/day10/src/main.rs
@@ -1,30 +1,33 @@
+//Don't know if I like this usized all over the place...
+//if go back to u8, change *b as usize in parse_bytes to *b
+
 const PUZZLE: &'static str = include_str!("Input.txt");
 const BYTESPUZZLE: &[u8] = include_bytes!("Input.txt");
 
-fn parse_str(input: &str) -> Vec<u8> {
+fn parse_str(input: &str) -> Vec<usize> {
     input.split(",").map(|word| word.parse().expect("Failed to parse")).collect()    
 }
 
-fn nums() -> Vec<u8> {
+fn nums() -> Vec<usize> {
     (0..).take(256).collect()
 }
 
-fn parse_bytes(input: &'static [u8]) -> Vec<u8> {
-    let mut bytes: Vec<u8> = input.into_iter().map(|b| *b).collect();
+fn parse_bytes(input: &'static [u8]) -> Vec<usize> {
+    let mut bytes: Vec<usize> = input.into_iter().map(|b| *b as usize).collect();
     bytes.extend(&[17, 31, 73, 47, 23]);
     bytes
 }
 
-fn solve(nums: &mut [u8], lenghts: &[u8], current_pos: &mut usize, skipsize: &mut usize) -> i64 {
+fn solve(nums: &mut [usize], lenghts: &[usize], current_pos: &mut usize, skipsize: &mut usize) -> usize {
     let numslenght = nums.len();
     for len in lenghts.iter() {
         //the selected items from nums. wraps around. also gets the index.
-        let (indecies, selected): (Vec<usize>, Vec<u8>) = nums
+        let (indecies, selected): (Vec<usize>, Vec<usize>) = nums
                 .iter()
                 .enumerate()
                 .cycle()
                 .skip(*current_pos)
-                .take(*len as usize)
+                .take(*len)
                 .unzip();
             
         //loop over the indecies zipped with the reversed of the selected.
@@ -34,13 +37,13 @@ fn solve(nums: &mut [u8], lenghts: &[u8], current_pos: &mut usize, skipsize: &mu
             .zip(selected.into_iter().rev())
             .for_each(|(indecie, newnum)| nums[indecie] = newnum);
 
-        *current_pos += (*len as usize + *skipsize) % numslenght;
+        *current_pos += (*len + *skipsize) % numslenght;
         *skipsize += 1;
     }
-    nums[0] as i64 * nums[1] as i64
+    nums[0] * nums[1]
 }
 
-fn xor(nums: &[u8]) -> String {
+fn xor(nums: &[usize]) -> String {
     nums
         .chunks(16)
         .map(|chunk| chunk.iter().fold(0, |n, acc| n ^ acc))
