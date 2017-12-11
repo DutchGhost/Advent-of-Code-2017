26e54927358f77ca17bb58a4c45b932d364fccde
DutchGhost <kasper199914@gmail.com>
2017-12-11 20:28:50 +0100

    why doesnt this work?

--- Rust/day10/src/main.rs
+++ Rust/day10/src/main.rs
@@ -16,27 +16,31 @@ fn parse_bytes(input: &'static [u8]) -> Vec<usize> {
     input.into_iter().chain([17, 31, 73, 47, 23].iter()).map(|b| *b as usize).collect()
 }
 
-fn solve(nums: &mut [usize], lenghts: &[usize], current_pos: &mut usize, skipsize: &mut usize) -> usize {
+fn solve(rounds: i64, nums: &mut [usize], lenghts: &[usize], cpos: &mut usize, skipsize: &mut usize) -> usize {
     let numslenght = nums.len();
-    for len in lenghts.iter() {
-        //the selected items from nums. wraps around. also gets the index.
-        let (indecies, selected): (Vec<usize>, Vec<usize>) = nums
-                .iter()
-                .enumerate()
-                .cycle()
-                .skip(*current_pos)
-                .take(*len)
-                .unzip();
-            
-        //loop over the indecies zipped with the reversed of the selected.
-        //for each indecie, set nums[indecie] to newnum.
-        indecies
-            .into_iter()
-            .zip(selected.into_iter().rev())
-            .for_each(|(indecie, newnum)| nums[indecie] = newnum);
+    let mut indecies = Vec::with_capacity(200);
+    let mut selected = Vec::with_capacity(200);
+    for _ in 0..rounds {
+        for len in lenghts.iter() {
+            //the selected items from nums. wraps around. also gets the index.
+            (indecies, selected) = nums
+                    .iter()
+                    .enumerate()
+                    .cycle()
+                    .skip(*cpos)
+                    .take(*len)
+                    .unzip();
+                
+            //loop over the indecies zipped with the reversed of the selected.
+            //for each indecie, set nums[indecie] to newnum.
+            indecies
+                .drain(..)
+                .zip(selected.drain(..).rev())
+                .for_each(|(indecie, newnum)| nums[indecie] = newnum);
 
-        *current_pos += (*len + *skipsize) % numslenght;
-        *skipsize += 1;
+            *cpos += (*len + *skipsize) % numslenght;
+            *skipsize += 1;
+        }
     }
     nums[0] * nums[1]
 }
@@ -51,14 +55,14 @@ fn dense(nums: &[usize]) -> String {
 fn main() {
     let mut nums_part1 = nums();
     let lenghts_part1 = parse_str(PUZZLE);
-    println!("part 1: {}", solve(&mut nums_part1, &lenghts_part1, &mut 0, &mut 0));
+    println!("part 1: {}", solve(1, &mut nums_part1, &lenghts_part1, &mut 0, &mut 0));
 
     let mut current_pos = 0;
     let mut skipsize = 0;
 
     let mut nums_part2 = nums();
     let lenghts_part2 = parse_bytes(BYTESPUZZLE);
-    (0..64).for_each(|_| { solve(&mut nums_part2, &lenghts_part2, &mut current_pos, &mut skipsize);});
+    solve(64, &mut nums_part2, &lenghts_part2, &mut current_pos, &mut skipsize);
 
     println!("part 2: {}", dense(&nums_part2));
 }
