af55754f89744d2790485291ae662b71d898c7fb
DutchGhost <kasper199914@gmail.com>
2017-12-11 19:24:26 +0100

    Yay

--- Rust/day11/src/main.rs
+++ Rust/day11/src/main.rs
@@ -1,64 +1,59 @@
+use std::collections::HashMap;
 const PUZZLE: &'static str = include_str!("Input.txt");
+
+struct Hex<'a> {
+    map: HashMap<&'a str, (i64, i64)>,
+    iter: std::str::Split<'a, &'a str>,
+}
+
+impl<'a> Hex<'a> {
+    fn new(input: &'a str) -> Hex<'a> {
+        let mut map = HashMap::new();
+        map.insert("n", (0, 1));
+        map.insert("nw", (-1, 1));
+        map.insert("sw", (-1, 0));
+        map.insert("s", (0, -1));
+        map.insert("se", (1, -1));
+        map.insert("ne", (1, 0));
+
+        Hex {
+            map: map,
+            iter: input.split(","),
+        }
+    }
+}
+
+impl<'a> Iterator for Hex<'a> {
+    type Item = (i64, i64);
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            Some(ins) => self.map.get(&ins).cloned(),
+            None => None,
+        }
+    }
+}
+
+fn dist(x: i64, y: i64) -> i64 {
+    let z = -(x + y);
+    (x.abs()).max(y.abs()).max(z.abs())
+}
 fn solve(input: &str) -> (i64, i64) {
-    let mut x: i64 = 0;
-    let mut y: i64 = 0;
+    let (mut x, mut y) = (0, 0);
     let mut max = 0;
-    for ins in input.split(",") {
-        match ins {
-            "n" => {
-                y -= 1;
-            },
-            "nw" => {
-                x -= 1;
-            }
-            "sw" => {
-                x -= 1;
-                y += 1;
-            }
-            "s" => {
-                y += 1;
-            }
-            "se" => {
-                x += 1;
-            }
-            "ne" => {
-                x += 1;
-                y -= 1;
-            }
-            _ => panic!("INVALID"),
-        }
-        if y < 0 {
-            max = std::cmp::max(max, x);
-        }
-        else {
-            max = std::cmp::max(max, x + y);
-        }
+    let mut hex = Hex::new(input);
+    while let Some((newx, newy)) = hex.next() {
+        x += newx;
+        y += newy;
+        max = std::cmp::max(max, dist(x, y))
     }
-    if x.abs() * -1 == x {
-        println!("x is neg");
-        return (x.abs() - y, max)
-    }
-    if y.abs() * -1 == y {
-        println!("y is neg");
-        println!(" x plus y is {}", x + y);
-        return (x, max)
-    }
-    if x.abs() == y.abs() {
-        return (0, max)
-    }
-    if y < 0 {
-        return (x, max)
-    }
-    if x < 0 {
-        return (y, max)
-    }
-    return (x + y, max)
+    (dist(x, y), max)
 }
 
 fn main() {
     let (part1, part2) = solve(PUZZLE);
     println!("part 1: {}", part1);
-    println!("part 2: {}", part2);
+    println!("part 2: {}", part2)
+
 }
 
 mod tests {
