1ca85028664ea51470a16c2860c4c139038da3a2
DutchGhost <kasper199914@gmail.com>
2017-12-13 20:36:36 +0100

    Added rayon!

--- Rust/day13/src/main.rs
+++ Rust/day13/src/main.rs
@@ -1,18 +1,22 @@
+#![feature(test)]
 const PUZZLE: &'static str = include_str!("Input.txt");
+extern crate rayon;
+extern crate test;
 
-fn parse(input: &str) -> Vec<(i64, i64)> {
+use rayon::prelude::*;
+fn parse(input: &str) -> Vec<(i32, i32)> {
     input
         .lines()
         .map(|line| {
             let mut it = line.split(": ").map(|item| {
-                item.parse::<i64>().expect("Failed to parse")
+                item.parse::<i32>().expect("Failed to parse")
             });
             (it.next().unwrap(), it.next().unwrap())
         })
         .collect()
 }
 
-fn solve(firewall: &[(i64, i64)]) -> i64 {
+fn solve(firewall: &[(i32, i32)]) -> i32 {
     firewall
         .iter()
         .filter(|&&(depth, range)| depth % ((range - 1) * 2) == 0)
@@ -20,8 +24,27 @@ fn solve(firewall: &[(i64, i64)]) -> i64 {
         .sum()
 }
 
-fn solve2(firewall: &[(i64, i64)]) -> i64 {
-    (0..)
+fn par_solve(firewall: &[(i32, i32)]) -> i32 {
+    firewall
+        .par_iter()
+        .filter(|&&(depth, range)| depth % ((range - 1) * 2) == 0)
+        .map(|&(depth, range)| depth * range)
+        .sum()
+}
+
+
+fn par_solve2(firewall: &[(i32, i32)]) -> i32 {
+    (0..10_000_000).into_par_iter()
+        .find_first(|wait| {
+            firewall.iter().all(|&(depth, range)| {
+                (depth + wait) % ((range - 1) * 2) != 0
+            })
+        })
+        .unwrap()
+}
+
+fn solve2(firewall: &[(i32, i32)]) -> i32 {
+    (0..10_000_000)
         .find(|wait| {
             firewall.iter().all(|&(depth, range)| {
                 (depth + wait) % ((range - 1) * 2) != 0
@@ -30,8 +53,44 @@ fn solve2(firewall: &[(i64, i64)]) -> i64 {
         .unwrap()
 }
 
+
+
 fn main() {
-    let mut parsed = parse(PUZZLE);
+    let parsed = parse(PUZZLE);
+    
+    println!("{}", par_solve(&parsed));
+    println!("{}", par_solve2(&parsed));
+
     println!("{}", solve(&parsed));
     println!("{}", solve2(&parsed));
+}
+
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+    use super::*;
+    #[bench]
+    fn par_solve_2(b: &mut Bencher) {
+        let parsed = parse(PUZZLE);
+        b.iter(|| par_solve2(&parsed))
+    }
+
+    #[bench]
+    fn single_solve_2(b: &mut Bencher) {
+        let parsed = parse(PUZZLE);
+        b.iter(|| solve2(&parsed));
+    }
+
+    #[bench]
+    fn par_solve_1(b: &mut Bencher) {
+        let parsed = parse(PUZZLE);
+        b.iter(|| par_solve(&parsed));
+    }
+
+    #[bench]
+    fn singe_solve_1(b: &mut Bencher) {
+        let parsed = parse(PUZZLE);
+        b.iter(|| solve(&parsed));
+    }
+
 }
