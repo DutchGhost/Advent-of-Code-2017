1fe3e9507dc94b70f8029dcdeed6cb0e6987645a
DutchGhost <kasper199914@gmail.com>
2017-12-14 14:45:57 +0100

    also part 2. It's ugly

--- Rust/day14/src/main.rs
+++ Rust/day14/src/main.rs
@@ -24,7 +24,8 @@ fn wrapping(cpos: usize, len: usize, numslenght: usize) -> Wrapper {
         ))
     }
 }
-fn solve(lenghts: &[usize]) -> u32 {
+//return the 16 bytes at the end, so we can do both part 1 and part 2 with this function.
+fn solve(lenghts: &[usize]) -> Vec<usize> {
     let mut nums =  (0..).take(256).collect::<Vec<usize>>();
     let mut cpos = 0;
     let mut skipsize = 0;
@@ -47,14 +48,48 @@ fn solve(lenghts: &[usize]) -> u32 {
     }
     nums.chunks(16)
         .map(|chunk| chunk.iter().fold(0, |n, acc| n ^ acc))
-        .map(|i| i.count_ones())
-        .sum::<u32>()
+        .collect()
+}
+
+fn part2() {
+    let mut grid = Vec::new();
+    for y in 0..128 {
+        let str_to_hash = format!("{}-{}", PUZZLE, y);
+        let bytes_vec = str_to_hash.as_bytes().iter().chain(SALT.iter()).map(|b| *b as usize).collect::<Vec<_>>();
+        let bin = solve(&bytes_vec);
+        let binairy = bin.iter().map(|n| format!("{:08b}", n)).collect::<String>();
+        let bitvec = binairy.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>();
+        grid.push(bitvec);
+    }
+    
+    let mut s = 0;
+    for j in 0..grid.len() {
+        for i in 0..grid[j].len() {
+            s += wipe_region(&mut grid[..], j, i);
+        }
+    }
+    println!("{}", s);
+}
+
+fn wipe_region(v: &mut [Vec<u32>], r: usize, c: usize) -> i64 {
+    if r < 0 || c < 0 || r >= v.len() || c >= v[r].len() || v[r][c] == 0 {
+        return 0;
+    }
+    v[r][c] = 0;
+    wipe_region(v, r + 1, c);
+    wipe_region(v, r, c + 1);
+    wipe_region(v, r - 1, c);
+    wipe_region(v, r, c - 1);
+    return 1;
 }
 
 fn main() {
-    println!("{}", (0..128)
+    println!("part 1: {}", (0..128)
         .map(|n| format!("{}-{}", PUZZLE, n))
         .map(|s| s.as_bytes().iter().chain(SALT.iter()).map(|b| *b as usize).collect::<Vec<_>>())
         .map(|saltedbytes| solve(&saltedbytes))
-        .sum::<u32>())
+        .map(|hash| hash.iter().map(|i| i.count_ones()).sum::<u32>())
+        .sum::<u32>());
+
+    part2()
 }
