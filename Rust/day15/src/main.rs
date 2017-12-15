b80e45b5744714432bf83f06448709da88fc3779
DutchGhost <kasper199914@gmail.com>
2017-12-15 13:24:59 +0100

    xor...so much faster!

--- Rust/day15/src/main.rs
+++ Rust/day15/src/main.rs
@@ -24,13 +24,12 @@ impl Generator {
         }
     }
     fn next_with_devide(&mut self) -> usize {
-        loop {
-            if let Some(nxt) = self.next() {
-                if nxt % self.can_devide == 0 {
-                    return nxt;
-                }
+        while let Some(nxt) = self.next() {
+            if nxt % self.can_devide == 0 {
+                return nxt
             }
         }
+        0
     }
 }
 //maybe swap the remain division.
@@ -38,13 +37,10 @@ impl Iterator for Generator {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.value = (self.value * self.multiplier) % DEVIDER;
+        self.value *= self.multiplier;
+        self.value %= DEVIDER;
         Some(self.value)
     }
-}
-
-fn compare_bin_string(string1: String, string2: String) -> bool {
-    string1.chars().rev().zip(string2.chars().rev()).take(16).all(|(c1, c2)| c1 == c2)
 }
 
 fn part1() -> usize {
@@ -52,18 +48,7 @@ fn part1() -> usize {
     let mut generator_a = Generator::new(INPUT_A, VALUE_A, 0);
     let mut generator_b = Generator::new(INPUT_B, VALUE_B, 0);
 
-    for _ in 0..40_000_000 {
-        let next_a = generator_a.next().unwrap();
-        let next_b = generator_b.next().unwrap();
-
-        let stringa = format!("{:032b}", next_a);
-        let stringb = format!("{:032b}", next_b);
-
-        if compare_bin_string(stringa, stringb) {
-            sum += 1;
-        }
-    }
-    sum
+    generator_a.zip(generator_b).take(40_000_000).filter(|&(next_a, next_b)| (next_a ^ next_b) % 65536 == 0).count()
 }
 
 fn part2() -> usize {
@@ -74,10 +59,7 @@ fn part2() -> usize {
     for _ in 0..5_000_000 {
         let (next_a, next_b) = rayon::join(||generator_a.next_with_devide(),|| generator_b.next_with_devide());
         
-        let stringa = format!("{:032b}", next_a);
-        let stringb = format!("{:032b}", next_b);
-
-        if compare_bin_string(stringa, stringb) {
+        if (next_a ^ next_b) % 65536 == 0 {
             sum += 1;
         }
     }
