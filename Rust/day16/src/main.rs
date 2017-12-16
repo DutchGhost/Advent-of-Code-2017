1c97e2b6feafbb8d385a3f8de33768d8fed4f09e
DutchGhost <kasper199914@gmail.com>
2017-12-16 13:54:05 +0100

    made the cyclefinder nicer

--- Rust/day16/src/main.rs
+++ Rust/day16/src/main.rs
@@ -2,41 +2,40 @@
 const PUZZLE: &'static str = include_str!("Input.txt");
 const PROGRAMMS: [&str; 16] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
 
-fn run(programms: &mut [&str], ins: &str) {
-    if ins.starts_with("s") {
-        let n = programms.len() - ins[1..].parse::<usize>().unwrap();
-        programms.rotate(n);
-    }
-    else if ins.starts_with("x") {
-        let toswap = ins[1..].split("/").map(|pos| pos.parse::<usize>().unwrap()).collect::<Vec<_>>();
-        programms.swap(toswap[0], toswap[1]);
-    }
-    else {
-        let toswap = ins[1..].split("/").collect::<Vec<_>>();
+fn run(programms: &mut [&str], input: &str) {
+    for ins in input.split(",") {
+        if ins.starts_with("s") {
+            let n = programms.len() - ins[1..].parse::<usize>().unwrap();
+            programms.rotate(n);
+        }
+        else if ins.starts_with("x") {
+            let mut toswap = ins[1..].split("/").map(|pos| pos.parse::<usize>().unwrap());
+            programms.swap(toswap.next().unwrap(), toswap.next().unwrap());
+        }
+        else {
+            let toswap = ins[1..].split("/").collect::<Vec<_>>();
+            
+            let pos1 = programms.iter().position(|programm| programm == &toswap[0]).unwrap();
+            let pos2 = programms.iter().position(|programm| programm == &toswap[1]).unwrap();
 
-        let pos1 = programms.iter().position(|programm| programm == &toswap[0]).unwrap();
-        let pos2 = programms.iter().position(|programm| programm == &toswap[1]).unwrap();
-
-        programms.swap(pos1, pos2);
+            programms.swap(pos1, pos2);
+        }
     }
 }
 
-//make this better!
-fn get_cycle() -> usize {
+//runs the dance untill the thing repeat's itself.
+//returns after how many dances it repeats itself, and the programms.
+fn get_cycle<'a>() -> (usize, [&'a str; 16]) {
     let mut programms = PROGRAMMS;
-    let mut cache = Vec::new();
-    for i in 0.. {
-        for ins in PUZZLE.split(",") {
-            run(&mut programms, ins);
-        }
-        if cache.contains(&programms) {
-            return i;
-        }
-        else {
-            cache.push(programms.clone());
-        }
+    let mut cache = Vec::with_capacity(100);
+
+    let mut n = 0;
+    while !cache.contains(&programms) {
+        cache.push(programms.clone());
+        run(&mut programms, PUZZLE);
+        n += 1;
     }
-    unreachable!()
+    (n, programms)
 }
 
 fn stringify(programm: [&str; 16]) -> String {
@@ -44,16 +43,12 @@ fn stringify(programm: [&str; 16]) -> String {
 }
 fn main() {
     let mut programms = PROGRAMMS;
-    for ins in PUZZLE.split(",") {
-        run(&mut programms, ins);
-    }
+    run(&mut programms, PUZZLE);
     println!("part 1: {}", stringify(programms));
     
-    let mut programms = PROGRAMMS;
-    for _ in 0..(1000000000 % get_cycle()) {
-        for ins in PUZZLE.split(",") {
-            run(&mut programms, ins);
-        }
+    let (cycle, mut programms) = get_cycle();
+    for _ in 0..(1_000_000_000 % cycle) {
+        run(&mut programms, PUZZLE);
     }
     println!("part 2: {}", stringify(programms));
 }
