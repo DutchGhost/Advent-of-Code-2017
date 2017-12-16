563ad2c2c0268ef66ced8675914160f3b5dbd4f1
DutchGhost <kasper199914@gmail.com>
2017-12-16 12:48:05 +0100

    day 16 part 1 and 2!

--- Rust/day16/src/main.rs
+++ Rust/day16/src/main.rs
@@ -1,29 +1,59 @@
 #![feature(slice_rotate)]
 const PUZZLE: &'static str = include_str!("Input.txt");
+const PROGRAMMS: [&str; 16] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
 
-fn main() {
-    let mut programms = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
-    for ins in PUZZLE.split(",") {
-        if ins.starts_with("s") {
-            let mut n = programms.len() - ins[1..].parse::<usize>().unwrap();
-            programms.rotate(n);
+fn run(programms: &mut [&str], ins: &str) {
+    if ins.starts_with("s") {
+        let n = programms.len() - ins[1..].parse::<usize>().unwrap();
+        programms.rotate(n);
+    }
+    else if ins.starts_with("x") {
+        let toswap = ins[1..].split("/").map(|pos| pos.parse::<usize>().unwrap()).collect::<Vec<_>>();
+        programms.swap(toswap[0], toswap[1]);
+    }
+    else {
+        let toswap = ins[1..].split("/").collect::<Vec<_>>();
+
+        let pos1 = programms.iter().position(|programm| programm == &toswap[0]).unwrap();
+        let pos2 = programms.iter().position(|programm| programm == &toswap[1]).unwrap();
+
+        programms.swap(pos1, pos2);
+    }
+}
+
+//make this better!
+fn get_cycle() -> usize {
+    let mut programms = PROGRAMMS;
+    let mut cache = Vec::new();
+    for i in 0.. {
+        for ins in PUZZLE.split(",") {
+            run(&mut programms, ins);
         }
-        else if ins.starts_with("x") {
-            let toswap = ins[1..].split("/").map(|pos| pos.parse::<usize>().unwrap()).collect::<Vec<_>>();
-
-            programms.swap(toswap[0], toswap[1]);
-
+        if cache.contains(&programms) {
+            return i;
         }
         else {
-            let toswap = ins[1..].split("/").collect::<Vec<_>>();
-
-            let pos1 = programms.iter().position(|programm| programm == &toswap[0]).unwrap();
-            let pos2 = programms.iter().position(|programm| programm == &toswap[1]).unwrap();
-
-            programms.swap(pos1, pos2);
+            cache.push(programms.clone());
         }
     }
-    for letter in programms.iter() {
-        print!("{}", letter);
+    unreachable!()
+}
+
+fn stringify(programm: [&str; 16]) -> String {
+    programm.into_iter().map(|s| s.to_string()).collect::<String>()
+}
+fn main() {
+    let mut programms = PROGRAMMS;
+    for ins in PUZZLE.split(",") {
+        run(&mut programms, ins);
     }
+    println!("part 1: {}", stringify(programms));
+    
+    let mut programms = PROGRAMMS;
+    for _ in 0..(1000000000 % get_cycle()) {
+        for ins in PUZZLE.split(",") {
+            run(&mut programms, ins);
+        }
+    }
+    println!("part 2: {}", stringify(programms));
 }
