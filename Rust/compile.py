ba96bc304d9f8dbeeb06b79def9781da89fff161
DutchGhost <kasper199914@gmail.com>
2017-12-20 22:12:13 +0100

    added what day is being compiled

--- Rust/compile.py
+++ Rust/compile.py
@@ -3,6 +3,7 @@ from subprocess import run
 if __name__ == '__main__':
     for d in os.listdir(os.getcwd()):
         if d != 'compile.py':
+            print("going to compile {}".format(d))
             os.chdir(r"{}".format(d))
             run(["cargo", "rustc", "--release", "--", "-C", "target-cpu=native", "-C", "lto"])
             os.chdir('..')
