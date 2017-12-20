3572a7484d2c7cc0a1541658eb1753a2701b4479
DutchGhost <kasper199914@gmail.com>
2017-12-20 13:35:29 +0100

    cargo fmt

--- Rust/day20/src/particle.rs
+++ Rust/day20/src/particle.rs
@@ -2,8 +2,8 @@ use std::str::FromStr;
 
 fn to_nums<I, F>(iter: I, filter: F) -> (i64, i64, i64)
 where
-    I: Iterator<Item=char>,
-    F: Fn(&char) -> bool
+    I: Iterator<Item = char>,
+    F: Fn(&char) -> bool,
 {
     let mut stringified = iter.filter(filter).collect::<String>();
     let mut it = stringified.split(",");
@@ -17,7 +17,7 @@ where
 struct Position {
     x: i64,
     y: i64,
-    z: i64
+    z: i64,
 }
 
 #[derive(Debug)]
@@ -38,43 +38,36 @@ struct Acceleration {
 pub struct Particle {
     position: Position,
     velocity: Velocity,
-    acceleration: Acceleration
+    acceleration: Acceleration,
 }
 
 impl FromStr for Position {
     type Err = ();
     fn from_str(s: &str) -> Result<Position, Self::Err> {
-        let (x, y, z) = to_nums(s.chars(), |c| !(c == &'p' || c == &'=' || c == &'<' || c == &'>'));
-        Ok(Position {
-            x: x,
-            y: y,
-            z: z,
-        })
-
+        let (x, y, z) = to_nums(s.chars(), |c| {
+            !(c == &'p' || c == &'=' || c == &'<' || c == &'>')
+        });
+        Ok(Position { x: x, y: y, z: z })
     }
 }
 
 impl FromStr for Velocity {
     type Err = ();
     fn from_str(s: &str) -> Result<Velocity, Self::Err> {
-        let (x, y, z) = to_nums(s.chars(), |c| !(c == &'v' || c == &'=' || c == &'<' || c == &'>'));
-        Ok(Velocity {
-            x: x,
-            y: y,
-            z: z,
-        })
+        let (x, y, z) = to_nums(s.chars(), |c| {
+            !(c == &'v' || c == &'=' || c == &'<' || c == &'>')
+        });
+        Ok(Velocity { x: x, y: y, z: z })
     }
 }
 
 impl FromStr for Acceleration {
     type Err = ();
     fn from_str(s: &str) -> Result<Acceleration, Self::Err> {
-        let (x, y, z) = to_nums(s.chars(), |c| !(c == &'a' || c == &'=' || c == &'<' || c == &'>'));
-        Ok(Acceleration {
-            x: x,
-            y: y,
-            z: z,
-        })
+        let (x, y, z) = to_nums(s.chars(), |c| {
+            !(c == &'a' || c == &'=' || c == &'<' || c == &'>')
+        });
+        Ok(Acceleration { x: x, y: y, z: z })
     }
 }
 
@@ -82,10 +75,10 @@ impl FromStr for Particle {
     type Err = ();
     fn from_str(s: &str) -> Result<Particle, Self::Err> {
         let mut pos_vel_acc = s.split(", ");
-        
+
         let p = pos_vel_acc.next().unwrap();
         let pos = Position::from_str(p).unwrap();
-        
+
         let v = pos_vel_acc.next().unwrap();
         let vel = Velocity::from_str(v).unwrap();
 
@@ -99,5 +92,3 @@ impl FromStr for Particle {
         })
     }
 }
-
-
