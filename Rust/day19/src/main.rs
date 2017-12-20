75f2d7485ae08dc73f215e6c812b8726ef151646
DutchGhost <kasper199914@gmail.com>
2017-12-20 20:26:30 +0100

    mhhh, still a bit weird

--- Rust/day19/src/main.rs
+++ Rust/day19/src/main.rs
@@ -1,6 +1,6 @@
 const PUZZLE: &'static str = include_str!("Input.txt");
 mod node;
-use node::{Node, Coordinates};
+use node::{Node, Walker};
 
 fn parse(input: &str) -> Vec<Vec<Node>> {
     input
@@ -12,12 +12,12 @@ fn parse(input: &str) -> Vec<Vec<Node>> {
 fn main() {
     let nodes = parse(PUZZLE);
     let mut n = 0;
-    let mut coordinates = Coordinates::new(nodes);
+    let mut walker = Walker::new(nodes);
 
-    while !coordinates.voidnode() {
-        coordinates.walk();
+    while !walker.voidnode() {
+        walker.walk();
         n += 1;
     }
-    println!("part 1: {}", coordinates.getstr());
+    println!("part 1: {}", walker.getstr());
     println!("part 2: {}", n);
 }
--- Rust/day19/src/node.rs
+++ Rust/day19/src/node.rs
@@ -28,24 +28,24 @@ enum Direction {
 }
 
 #[derive(Debug)]
-pub struct Coordinates {
-    x: i64,
-    y: i64,
+pub struct Walker {
+    x: usize,
+    y: usize,
     direction: Direction,
     message: String,
     nodes: Vec<Vec<Node>>
 }
 
-impl Coordinates {
-    ///A new node is initalized with just the x set to where you enter the maze.
-    pub fn new(nodes: Vec<Vec<Node>>) -> Coordinates {
+impl Walker {
+    ///A new Walker is initalized with just the x set to where you enter the maze.
+    pub fn new(nodes: Vec<Vec<Node>>) -> Walker {
         let x = nodes[0]
             .iter()
             .position(|node| node == &Node::Pipe)
             .unwrap();
 
-        Coordinates {
-            x: x as i64,
+        Walker {
+            x: x,
             y: 0,
             direction: Direction::Down,
             message: String::new(),
@@ -58,7 +58,7 @@ impl Coordinates {
     }
     
     pub fn voidnode(&self) -> bool {
-        self.nodes[self.y as usize][self.x as usize] == Node::Void
+        self.nodes[self.y][self.x] == Node::Void
     }
 
     ///first walk, then check the node.
@@ -75,46 +75,48 @@ impl Coordinates {
     }
 
     pub fn checknode(&mut self) {
-        match self.nodes[self.y as usize][self.x as usize] {
+        match self.nodes[self.y][self.x] {
             Node::Letter(c) => self.message.push(c),
             Node::Turn => self.turn(),
             _ => return,
         }
     }
 
-    //if we need to change, try to look in one way. If that succeeds, turn that way.
-    //if it doesn't, turn the other.
-    //also, checks if you even CAN look in a way, if you even't can't look in a way,
-    //turn the other.
+    //return None if you can't even look to the right / down
+    //return None if x + 1 or y + 1 equals Node::Void.
+    fn node_at_pos(&self, s: &str) -> Option<()> {
+        match s {
+            "updown" => {
+                if self.x + 1 >= self.nodes[0].len() || self.nodes[self.y][self.x + 1] == Node::Void {
+                    None
+                }
+                else {
+                    Some(())
+                }
+            }
+            "leftright" => {
+                if self.y > self.nodes.len() || self.nodes[self.y + 1][self.x] == Node::Void {
+                    None
+                }
+                else {
+                    Some(())
+                }
+            }
+            _ => panic!()
+        }
+    }
     pub fn turn(&mut self) {
         match self.direction {
             Direction::Up | Direction::Down => {
-                
-                //can't even look to the right!
-                if (self.x + 1) >= self.nodes[0].len() as i64 {
-                    self.direction = Direction::Left
-                }
-                //if it's not a void..., turn right.
-                else if self.nodes[self.y as usize][(self.x + 1) as usize] != Node::Void {
-                    self.direction = Direction::Right;
-                }
-                //it was a void! turn left
-                else {
-                    self.direction = Direction::Left;
+                match self.node_at_pos("updown") {
+                    Some(_) => self.direction = Direction::Right,
+                    None => self.direction = Direction::Left,
                 }
             },
             Direction::Left | Direction::Right => {
-                //can't even look down!
-                if (self.y + 1) >= self.nodes.len() as i64 {
-                    self.direction = Direction::Up;
-                }
-                //if it's not a void..., turn down
-                else if self.nodes[(self.y + 1) as usize][self.x as usize] != Node::Void {
-                    self.direction = Direction::Down;
-                }
-                //it was a void! turn up!
-                else {
-                    self.direction = Direction::Up;
+                match self.node_at_pos("leftright") {
+                    Some(_) => self.direction = Direction::Down,
+                    None => self.direction = Direction::Up,
                 }
             },
         }
