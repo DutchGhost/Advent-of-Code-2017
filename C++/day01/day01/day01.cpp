cd742f8dc61f2836e8b546d7e2e60417ce49179f
DutchGhost <kasper199914@gmail.com>
2017-12-21 19:34:48 +0100

    also added the 'optimized' version, and changed return of int to return of uint32_t

--- C++/day01/day01/day01.cpp
+++ C++/day01/day01/day01.cpp
@@ -3,15 +3,26 @@
 #include <sstream>
 using namespace std;
 
-int summenize(string Input, uint32_t skip) {
+uint32_t summenize(string Input, uint32_t skip) {
 	int totall = 0;
 
-	for (uint32_t i = 0; i < (Input).length(); i++) {
+	for (uint32_t i = 0; i < Input.length(); i++) {
 		if (Input[i] == Input[(i + skip) % Input.length()]) {
 			totall += (Input[i] - '0');
 		}
 	}
 	return totall;
+}
+
+uint32_t optimized(string Input, uint32_t skip) {
+	int totall = 0;
+
+	for (uint32_t i = 0; i < Input.length() >> 1; i++) {
+		if (Input[i] == Input[(i + skip) % Input.length()]) {
+			totall += (Input[i] - '0');
+		}
+	}
+	return totall << 1;
 }
 
 string readfile() {
@@ -28,5 +39,5 @@ const string PUZZLE = readfile();
 int main() {
 	
 	cout << summenize(PUZZLE, 1) << endl;
-	cout << summenize(PUZZLE, PUZZLE.length() / 2) << endl;
+	cout << optimized(PUZZLE, PUZZLE.length() / 2) << endl;
 }
