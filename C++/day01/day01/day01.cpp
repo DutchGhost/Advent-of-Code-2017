addfb88827e2f5a3d4e6ddbcd238108c27b5ac53
DutchGhost <kasper199914@gmail.com>
2017-12-21 17:14:32 +0100

    made it a const!

--- C++/day01/day01/day01.cpp
+++ C++/day01/day01/day01.cpp
@@ -3,25 +3,30 @@
 #include <sstream>
 using namespace std;
 
-int summenize(string* str, uint32_t skip) {
+int summenize(string Input, uint32_t skip) {
 	int totall = 0;
 
-	for (uint32_t i = 0; i < (*str).length(); i++) {
-		if ((*str)[i] == (*str)[(i + skip) % (*str).length()]) {
-			totall += ((*str)[i] - '0');
+	for (uint32_t i = 0; i < (Input).length(); i++) {
+		if (Input[i] == Input[(i + skip) % Input.length()]) {
+			totall += (Input[i] - '0');
 		}
 	}
 	return totall;
 }
 
-int main() {
+string readfile() {
 	ifstream f;
 	f.open("Input.txt");
 	stringstream buffer;
 
 	buffer << f.rdbuf();
 	string str = buffer.str();
+	return str;
+}
+
+const string PUZZLE = readfile();
+int main() {
 	
-	cout << summenize(&str, 1) << endl;
-	cout << summenize(&str, str.length() / 2) << endl;
+	cout << summenize(PUZZLE, 1) << endl;
+	cout << summenize(PUZZLE, PUZZLE.length() / 2) << endl;
 }
