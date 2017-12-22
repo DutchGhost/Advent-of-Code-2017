034ef133cab972ad54fad029c3605c32bddb44b3
DutchGhost <kasper199914@gmail.com>
2017-12-22 17:53:32 +0100

    rewrote the damn thing

--- Rust/day22/src/main.rs
+++ Rust/day22/src/main.rs
@@ -1,18 +1,12 @@
 const PUZZLE: &'static str = include_str!("Input.txt");
 
 mod walker;
-mod walker2;
-mod tmp;
 use walker::*;
-use walker2::*;
 
 fn main() {
-    let nodes1 = PUZZLE.lines().map(|line| line.chars().map(|c| Node::from(c)).collect::<Vec<_>>()).collect::<Vec<_>>();
-    let nodes2 = PUZZLE.lines().map(|line| line.chars().map(|c| Node2::from(c)).collect::<Vec<_>>()).collect::<Vec<_>>();
-    let walker = Walker::new(nodes1);
-    let walker2 = Walker2::new(nodes2);
-
-    println!("{}", walker.take(10_000).sum::<i64>());
-    println!("{}", walker2.take(10_000_000).sum::<i64>());
-
+    let walker = Walker::new(PUZZLE, Part::Part1);
+    let walker2 = Walker::new(PUZZLE, Part::Part2);
+    
+    println!("{}", walker.take(10000).sum::<i32>());
+    println!("{}", walker2.take(10_000_000).sum::<i32>());
 }
--- Rust/day22/src/walker.rs
+++ Rust/day22/src/walker.rs
@@ -1,146 +1,242 @@
-use std::mem;
+use std::str::FromStr;
 
 #[derive(Debug, Clone)]
 pub enum Node {
-    Infected,
     Clean,
+    Weakened,
+    Infected,
+    Flagged,
 }
 
 impl From<char> for Node {
-    fn from(c: char) -> Self {
+    
+    #[inline]
+    fn from(c: char) -> Node {
         match c {
             '.' => Node::Clean,
             '#' => Node::Infected,
-            _ => panic!("I don't know this kind of node")
+            _ => panic!("Invalid nodetype.")
         }
     }
 }
 
 #[derive(Debug)]
-pub enum Direction {
-    Right,
-    Up,
-    Down,
-    Left,
-}
-
-impl Direction {
-    fn turn_right(&mut self) {
-        mem::replace(self, match self {
-            &mut Direction::Right => Direction::Down,
-            &mut Direction::Down => Direction::Left,
-            &mut Direction::Left => Direction::Up,
-            &mut Direction::Up => Direction::Right,
-        });
-    }
-    fn turn_left(&mut self) {
-        mem::replace(self, match self {
-            &mut Direction::Left => Direction::Down,
-            &mut Direction::Down => Direction::Right,
-            &mut Direction::Right => Direction::Up,
-            &mut Direction::Up => Direction::Left,
-        });
-    }
-}
-
-#[derive(Debug)]
-pub struct Pos {
+struct Position {
     x: usize,
     y: usize,
 }
 
-impl Pos {
-    fn new() -> Pos {
-        Pos {
-            x: 12,
-            y: 12,
+impl From<(usize, usize)> for Position {
+    
+    #[inline]
+    fn from((n1, n2): (usize, usize)) -> Position {
+        Position {x: n1, y: n2}
+    }
+}
+
+struct Grid {
+    grid: Vec<Vec<Node>>
+}
+
+impl FromStr for Grid {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Grid{ grid: s.lines().map(|line| line.chars().map(|c| Node::from(c)).collect()).collect()})
+    }
+}
+
+impl Grid {
+    fn node_at_pos<'m, 's: 'm>(&'s mut self, pos: &Position) -> Option<&'m mut Node> {
+        self.grid.get_mut(pos.y).and_then(|row| row.get_mut(pos.x))
+    }
+
+    #[inline]
+    fn extend_left(&mut self) {
+        for mut row in self.grid.iter_mut() {
+            row.insert(0, Node::Clean);
         }
     }
+
+    #[inline]
+    fn extend_right(&mut self) {
+        for row in self.grid.iter_mut() {
+            row.push(Node::Clean);
+        }
+    }
+
+    #[inline]
+    fn extend_top(&mut self) {
+        let len = self.grid[0].len();
+        self.grid.insert(0, vec![Node::Clean; len])
+    }
+
+    #[inline]
+    fn extend_bottem(&mut self) {
+        let len = self.grid[0].len();
+        self.grid.push(vec![Node::Clean; len])
+    }
+
+}
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    Init,
+}
+
+impl Direction {
+    
+    #[inline]
+    fn turn_right(&self) -> Self {
+        match *self {
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Up => Direction::Right,
+            Direction::Init => Direction::Right,
+        }
+    }
+
+    #[inline]
+    fn turn_left(&self) -> Self {
+        match *self {
+            Direction::Left => Direction::Down,
+            Direction::Down => Direction::Right,
+            Direction::Right => Direction::Up,
+            Direction::Up => Direction::Left,
+            Direction::Init => Direction::Left,
+        }
+    }
+
+    #[inline]
+    fn reverse(&self) -> Self {
+        match *self {
+            Direction::Left => Direction::Right,
+            Direction::Down => Direction::Up,
+            Direction::Right => Direction::Left,
+            Direction::Up => Direction::Down,
+            Direction::Init => panic!("this should never happen.")
+        }
+    }
+}
+
+pub enum Part {
+    Part1,
+    Part2,
 }
 
 pub struct Walker {
-    pos: Pos,
-    grid: Vec<Vec<Node>>,
-    direction: Direction,
+    grid: Grid,
+    pos: Position,
+    facing: Direction,
+    part: Part,
 }
 
-//maybe change the direction for infected??
 impl Walker {
-    pub fn new(nodes: Vec<Vec<Node>>) -> Walker {
-        let p = Pos::new();
+    pub fn new(s: &str, part: Part) -> Walker {
+        let grid = Grid::from_str(s).unwrap();
 
-        let dir = match nodes[p.y][p.x] {
-            Node::Clean => Direction::Up,
-            Node::Infected => Direction::Down
-        };
-        
+        let middle = grid.grid.len() / 2;
+
+
         Walker {
-            pos: p,
-            grid: nodes,
-            direction: dir,
+            grid: grid,
+            pos: Position::from((middle, middle)),
+            facing: Direction::Init,
+            part: part,
+        }
+    }
+    fn step(&mut self) {
+        match self.facing {
+            Direction::Up => self.pos.y -= 1,
+            Direction::Down => self.pos.y += 1,
+            Direction::Left => self.pos.x -= 1,
+            Direction::Right => self.pos.x += 1,
+            Direction::Init => panic!("This hould never happen!"),
         }
     }
 
-    fn expand(&mut self) {
-        if self.pos.x == 0 {
-            for mut row in self.grid.iter_mut() {
-                row.insert(0, Node::Clean);
-            }
-            self.pos.x += 1;
-        }
-        if self.pos.x == self.grid[0].len() {
-            for mut row in self.grid.iter_mut() {
-                row.push(Node::Clean);
-            }
-        }
-        if self.pos.y == 0 {
-            let len = self.grid[0].len();
-            let insert = vec![Node::Clean; len];
-            self.grid.insert(0, insert);
-            self.pos.y += 1;
-        }
-
-        if self.pos.y == self.grid.len() {
-            let len = self.grid[0].len();
-            let insert = vec![Node::Clean; len];
-            self.grid.push(insert);
-        }
-    }
-}
-//liftimes :). Sad this has te be outside the impl block... :(, else there are 2 &mut's
-fn node_at_pos<'m, 's: 'm>(nodes: &'s mut [Vec<Node>], pos: &Pos) -> Option<&'m mut Node> {
-    nodes.get_mut(pos.y).unwrap().get_mut(pos.x)
-}
-
-impl Iterator for Walker {
-    type Item = i64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.expand();
-        
-        let infected = match node_at_pos(&mut self.grid, &self.pos) {
+    fn diagnostics(&mut self) -> i32 {
+        let ret = match self.grid.node_at_pos(&self.pos) {
             Some(n) => {
                 match n {
                     &mut Node::Clean => {
-                        self.direction.turn_left();
+                        self.facing = self.facing.turn_left();
+                        *n = Node::Infected;
+                        1
+                    },
+                    &mut Node::Infected => {
+                        self.facing = self.facing.turn_right();
+                        *n = Node::Clean;
+                        0
+                    }
+                    _ => panic!("cant happen on part 1!")
+                }
+            },
+            None => panic!("Something went terribly horribly wrong! {:?}", self.pos),
+        };
+        self.step();
+        return ret
+    }
+
+    fn advanced_diagnostics(&mut self) -> i32 {
+        let ret = match self.grid.node_at_pos(&self.pos) {
+            Some(n) => {
+                match n {
+                    &mut Node::Clean => {
+                        self.facing = self.facing.turn_left();
+                        *n = Node::Weakened;
+                        0
+                    },
+                    &mut Node::Weakened => {
                         *n = Node::Infected;
                         1
                     }
                     &mut Node::Infected => {
-                        self.direction.turn_right();
+                        self.facing = self.facing.turn_right();
+                        *n = Node::Flagged;
+                        0
+                    }
+                    &mut Node::Flagged => {
+                        self.facing = self.facing.reverse();
                         *n = Node::Clean;
                         0
                     }
                 }
-            }
-            None => panic!("something went horrible terribly wrong.")
+            },
+            None => panic!("Something went terribly horribly wrong!"),
         };
-        match self.direction {
-            Direction::Up => self.pos.y -= 1,
-            Direction::Down => self.pos.y += 1,
-            Direction::Right => self.pos.x += 1,
-            Direction::Left => self.pos.x -= 1,
-        };
-        Some(infected)
+        self.step();
+        return ret
+    }
+}
+
+impl Iterator for Walker {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        
+        if self.pos.x == 0 {
+            self.grid.extend_left();
+            self.pos.x += 1;
+        }
+        if self.pos.x == self.grid.grid[0].len() {
+            self.grid.extend_right();
+        }
+        if self.pos.y == 0 {
+            self.grid.extend_top();
+            self.pos.y += 1;
+        }
+        if self.pos.y == self.grid.grid.len() {
+            self.grid.extend_bottem();
+            //self.pos.y -= 1;
+        }
+        
+        match self.part {
+            Part::Part1 => Some(self.diagnostics()),
+            Part::Part2 => Some(self.advanced_diagnostics()),
+        }
     }
 }
