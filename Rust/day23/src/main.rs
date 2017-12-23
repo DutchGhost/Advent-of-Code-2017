997a6f37284daeaa933e7dc9563f7287b18e5b81
DutchGhost <kasper199914@gmail.com>
2017-12-23 11:56:01 +0100

    .entry().or_insert() is cool

--- Rust/day23/src/main.rs
+++ Rust/day23/src/main.rs
@@ -2,39 +2,33 @@ const PUZZLE: &'static str = include_str!("Input.txt");
 
 use std::collections::BTreeMap;
 
+fn parse(input: &str) -> Vec<Vec<&str>> {
+    input.lines().map(|line| line.split(" ").collect()).collect()
+}
+
 //returns either a value that needs top copied, or the value of a register
-fn read(s: &str, map: &BTreeMap<String, i64>) -> i64 {
+fn read<'b, 'a: 'b>(s: &'a str, map: &mut BTreeMap<&'b str, i64>) -> i64 {
     match s.parse::<i64>() {
         Ok(n) => n,
-        Err(_) => *map.get(s).unwrap(),
+        Err(_) => *map.entry(s).or_insert(0),
     }
 }
 
 fn main() {
-    let mut registers = vec![
-            ("a".to_string(), 0i64),
-            ("b".to_string(), 0i64),
-            ("c".to_string(), 0i64),
-            ("d".to_string(), 0i64),
-            ("e".to_string(), 0i64),
-            ("f".to_string(), 0i64),
-            ("g".to_string(), 0i64),
-            ("h".to_string(), 0i64)]
-        .into_iter()
-        .collect::<BTreeMap<String, i64>>();
-
-    let instructions: Vec<Vec<String>> = PUZZLE.lines()
-        .map(|s| s.split(" ").map(|ss| ss.to_owned()).collect())
-        .collect();
+    let mut registers = BTreeMap::new();
+    let instructions = parse(PUZZLE);
     let mut ip: i64 = 0;
     let mut muls = 0;
     while ip < instructions.len() as i64 {
         let ins = &instructions[ip as usize];
         match ins[0].as_ref() {
-            "set" => *registers.get_mut(&ins[1]).unwrap() = read(&ins[2], &registers),
-            "sub" => *registers.get_mut(&ins[1]).unwrap() -= read(&ins[2], &registers),
-            "mul" => {*registers.get_mut(&ins[1]).unwrap() *= read(&ins[2], &registers); muls += 1;},
-            "jnz" => if read(&ins[1], &registers) != 0 {ip += read(&ins[2], &registers)-1;},
+            "set" => *registers.entry(ins[1]).or_insert(0) = read(&ins[2], &mut registers),
+            "sub" => *registers.entry(ins[1]).or_insert(0) -= read(&ins[2], &mut registers),
+            "mul" => {
+                *registers.entry(ins[1]).or_insert(0) *= read(&ins[2], &mut registers);
+                muls += 1;
+            },
+            "jnz" => if read(&ins[1], &mut registers) != 0 {ip += read(&ins[2], &mut registers)-1;},
             _ => panic!(),
         }
         ip += 1;
