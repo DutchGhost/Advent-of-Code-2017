fe11106ece3bec7b7d71ebe62217a9b8ad22bfdb
DutchGhost <kasper199914@gmail.com>
2017-12-24 14:55:22 +0100

    mhh, let's think about this

--- Rust/day24/src/component.rs
+++ Rust/day24/src/component.rs
@@ -1,27 +1,31 @@
 use std::str::FromStr;
 
 #[derive(Debug, Hash, PartialEq, Eq)]
-pub struct Component {
-    front: i64,
-    back: i64,
+pub struct Port {
+    pub front: u8,
+    pub back: u8,
 }
 
-impl FromStr for Component {
+impl FromStr for Port {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut it = s.split(r"/");
 
-        Ok(Component { front: it.next().unwrap().parse().unwrap(), back: it.next().unwrap().parse().unwrap()})
+        Ok(Port { front: it.next().unwrap().parse().unwrap(), back: it.next().unwrap().parse().unwrap()})
     }
 }
 
-impl Component {
-    pub fn connectable(&self, other: &Component) -> bool {
-        self.back == other.front
+impl Port {
+    pub fn connectable(&self, other: &Port) -> bool {
+        self.back == other.front || self.back == other.back || self.front == other.front
     }
 
-    pub fn value(&self) -> i64 {
+    fn zeroport(&self) -> bool {
+        self.front == 0 || self.back == 0
+    }
+
+    pub fn value(&self) -> u8 {
         self.front + self.back
     }
 }
--- Rust/day24/src/main.rs
+++ Rust/day24/src/main.rs
@@ -1,69 +1,18 @@
-extern crate itertools;
-use itertools::*;
-
 const PUZZLE: &'static str = include_str!("Input.txt");
 
 mod component;
 use component::*;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use std::str::FromStr;
 
-fn parse(input: &str) -> Vec<Component> {
-    input.lines().map(|line| Component::from_str(line).unwrap()).collect()
-}
-
-fn make_comb_tree(components: &[Component]) -> HashMap<&Component, Vec<&Component>>{
-    let mut map = HashMap::new();
-    for (idx, component) in components.iter().enumerate() {
-        for comp2 in components[idx + 1..].iter() {
-            if component.connectable(&comp2) {
-                map.entry(component).or_insert(Vec::new()).push(comp2);
-            }
-        }
-    }
-
-    let mut secondmap = HashMap::new();
-
-    for (k, v) in map.iter() {
-        for component in v.iter() {
-            if let Some(comps) = map.get(component) {
-                let mut vclone = v.clone();
-                vclone.extend(comps.clone());
-                secondmap.insert(*k, vclone);
-            }
-            else {
-                secondmap.insert(*k, v.to_vec());
-            }
-        }
-    }
-
-    let mut thirdmap = HashMap::new();
-
-    for (k, v) in secondmap.iter() {
-        for component in v.iter() {
-            if let Some(comps) = map.get(component) {
-                let mut vclone = v.clone();
-                vclone.extend(comps.clone());
-                thirdmap.insert(*k, vclone);
-            }
-            else {
-                thirdmap.insert(*k, v.to_vec());
-            }
-        }
-    }
-
-    thirdmap
+fn parse(input: &str) -> Vec<Port> {
+    input.lines().map(|line| Port::from_str(line).unwrap()).collect()
 }
 
 
 fn main() {
     let mut components = parse(PUZZLE);
     
-    let map = make_comb_tree(&components);
-
-    for (k, v) in map.iter() {
-        println!("{:?} -> {:?}", k, v);
-    }
-}
+   }
