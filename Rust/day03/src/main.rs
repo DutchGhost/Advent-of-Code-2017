d9a34e8570bf42fd85409cd51c652c7c79a8706a
DutchGhost <kasper199914@gmail.com>
2018-01-07 20:02:33 +0100

    yay it works now with libaoc!

--- Rust/day03/src/main.rs
+++ Rust/day03/src/main.rs
@@ -1,13 +1,12 @@
 #![feature(conservative_impl_trait)]
 #![feature(generator_trait, generators)]
 
+extern crate libaoc;
+
 mod genitter;
 
 mod spiral;
 use spiral::Spiral;
-
-mod tmp;
-use tmp::Spiral as blah;
 
 fn main() {
     let input = 361527;
@@ -16,8 +15,4 @@ fn main() {
     println!("{}", spiral.part1(input));
     spiral.reset();
     println!("{}", spiral.part2(input));
-
-    let t = blah::new();
-
-    t.take(10).for_each(|p| println!("{:?}", p));
 }
