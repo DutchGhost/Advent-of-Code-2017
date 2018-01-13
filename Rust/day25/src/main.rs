2f68ed3e0d9bde84ecbaea21b297b0b13aa07d94
DutchGhost <kasper199914@gmail.com>
2018-01-13 22:37:56 +0100

    yay it works

--- Rust/day25/src/main.rs
+++ Rust/day25/src/main.rs
@@ -1,8 +1,11 @@
+const PUZZLE: &'static str = include_str!("Input.txt");
+use std::str::FromStr;
 mod state;
 use state::*;
+use state::Block;
 
 fn main() {
-    let mut cpu = CPU::new();
+    let mut cpu = CPU::new(PUZZLE.split("\n,").map(|block| Block::from_str(block).unwrap()).collect());
     for _ in 0..12794428 {
         cpu.run();
     }
