4729ce6d96c10a994f83d8252d23e9be44628748
DutchGhost <kasper199914@gmail.com>
2018-04-01 16:28:02 +0200

    now first runs cargo clean, to clear all builds, so there's no dangling deps

--- Rust/compile.py
+++ Rust/compile.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
         if directory != 'compile.py':
             print("going to compile {}".format(directory))
             os.chdir(directory)
+            run(["cargo", "clean"])
             run(["cargo", "update"])
             run(["cargo", "rustc", "--release", "--", "-C", "target-cpu=native", "-C", "lto"])
             os.chdir('..')
